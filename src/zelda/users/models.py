d7ed529c6149722aacdc295fad056dd812aa8ed0
Ruben Branco <ruben.branco@outlook.pt>
2019-03-02 19:35:32 +0000

    Added users with AppUser, Student, Professor and admin models.

---
+++ src/zelda/users/models.py
@@ -0,0 +1,86 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django_countries.fields import CountryField
+from django.utils.translation import gettext_lazy as _
+
+
+class AppUser(User):
+    MARITAL_STATUS = (
+        ("single", _("Single")),
+        ("married", _("Married")),
+        ("divorced", _("Divorced")),
+        ("widowed", _("Widowed")),
+    )
+
+    GENDER = (
+        ("m", _("Male")),
+        ("f", _("Female")),
+    )
+
+    nif = models.IntegerField()
+    n_cc = models.IntegerField()
+    dob = models.DateField()
+    institucional_email = models.EmailField()
+    contact = models.IntegerField()
+    emergency_contact = models.IntegerField()
+    professional_occupation = models.TextField(max_length=512)
+    display_image = models.ImageField()
+    country = CountryField()
+    marital_status = models.CharField(
+        max_length=8,
+        choices=MARITAL_STATUS,
+    )
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER,
+    )
+
+
+class Student(AppUser):
+    CALL = (
+        ("1", _("1st Call")),
+        ("2", _("2nd Call")),
+    )
+
+    STATUTE = (
+        ("worker", _("Worker")),
+        ("athlete", _("Athlete")),
+        ("fireman", _("Fireman")),
+        ("military", _("Military")),
+        ("union", _("Student's Union")),
+        ("special", _("Special Learning Needs")),
+        ("parent", _("Parent")),
+    )
+
+    number = models.IntegerField()
+    call = models.CharField(
+        max_length=1,
+        choices=CALL,
+    )
+    entry_grade = models.FloatField(max_length=200.0)
+    statute = models.CharField(
+        max_length=8,
+        choices=STATUTE,
+    )
+
+
+class Administrator(AppUser):
+    number = models.IntegerField()
+
+
+class Professor(AppUser):
+    RANKS = (
+        ("assist", _("Assistant Professor")),
+        ("assistinv", _("Invited Assistant Professor")),
+        ("full", _("Full Professor")),
+        ("fullvis", _("Visiting Full Professor")),
+        ("associ", _("Associate Professor")),
+        ("associnv", _("Invited Associate Professor")),
+        ("assocagr", _("Associate Professor with Agregação")),
+    )
+
+    number = models.IntegerField()
+    rank = models.CharField(
+        max_length=9,
+        choices=RANKS,
+    )
