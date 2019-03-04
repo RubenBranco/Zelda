eb45a5780bc22197e130991ce3e2a1d089576c6c
Ruben Branco <ruben.branco@outlook.pt>
2019-03-04 22:31:00 +0000

    Modelling Dept and council. Adding subjects to courses.

--- src/zelda/courses/models.py
+++ src/zelda/courses/models.py
@@ -1,6 +1,46 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from users.models import Professor
+from organizations.models import Department
+
+
+class SubjectSpecification(models.Model):
+    ects = models.FloatField()
+    designation = models.CharField(max_length=100)
+    code = models.CharField(max_length=10)
+    programme = models.TextField()
+    objectives = models.TextField()
+    evaluation_method = models.TextField()
+    bibliography = models.TextField()
+
+
+class Subject(models.Model):
+    SEMESTERS = (
+        ("1", "1"),
+        ("2", "2"),
+    )
+
+    subject_spec = models.ForeignKey(
+        SubjectSpecification,
+        on_delete=models.CASCADE,
+    )
+    lective_year = models.DateField()
+    semester = models.CharField(
+        max_length=1,
+        choices=SEMESTERS,
+    )
+    # WEEKS
+    duration = models.PositiveSmallIntegerField()
+
+
+class CourseSubject(models.Model):
+    subject = models.ForeignKey(
+        Subject,
+        on_delete=models.CASCADE,
+    )
+    course_year = models.PositiveSmallIntegerField()
+
 
 class EntranceExam(models.Model):
     designation = models.CharField(max_length=50)
@@ -40,18 +80,25 @@ class CourseSpecification(models.Model):
     )
     entrance_exams = models.ManyToManyField(
         EntranceExam,
-        null=True,
     )
     regime = models.CharField(
         max_length=5,
         choices=REGIME_OPTIONS,
     )
-
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE,
+    )
+    course_programme = models.ManyToManyField(CourseSubject)
 
 class Course(models.Model):
     specification = models.ForeignKey(
         CourseSpecification,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
     lective_year = models.DateField()
     vacancies = models.IntegerField()
+    coordinator = models.ForeignKey(
+        Professor,
+        on_delete=models.CASCADE,
+    )
--- src/zelda/organizations/models.py
+++ src/zelda/organizations/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from users.models import Professor
+
+
+class DepartmentCouncil(models.Model):
+    president = models.ForeignKey(
+        Professor,
+        related_name="president",
+        on_delete=models.CASCADE,
+    )
+    vice_president_one = models.ForeignKey(
+        Professor,
+        related_name="vice_president_one",
+        on_delete=models.CASCADE,
+    )
+    vice_president_two = models.ForeignKey(
+        Professor,
+        related_name="vice_president_two",
+        on_delete=models.CASCADE,
+    )
+
+
+class Department(models.Model):
+    designation = models.CharField(max_length=100)
+    field = models.CharField(max_length=50)
+    code = models.CharField(max_length=10)
+    council = models.ForeignKey(
+        DepartmentCouncil,
+        on_delete=models.CASCADE,
+    )
+
