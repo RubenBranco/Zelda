8625396510fc8d2d167afa265435663de2b3cd61
Ruben Branco <ruben.branco@outlook.pt>
2019-03-05 18:35:29 +0000

    Modelling classes, lessons, shifts.

--- src/zelda/timetable/models.py
+++ src/zelda/timetable/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from organizations.models import Room
+
+
+class LessonSpecification(models.Model):
+    WEEKDAYS = (
+        ("monday", _("Monday")),
+        ("tuesday", _("Tuesday")),
+        ("wednesday", _("Wednesday")),
+        ("thursday", _("Thursday")),
+        ("friday", _("Friday")),
+        ("saturday", _("Saturday")),
+        ("sunday", _("Sunday")),
+    )
+    LESSON_TYPE = (
+        ("theory", _("Theory")),
+        ("practice", _("Practice")),
+        ("lab", _("Laboratory")),
+        ("field", _("Field")),
+    )
+
+    weekday = models.CharField(
+        max_length=9,
+        choices=WEEKDAYS,
+    )
+    time = models.TimeField()
+    duration = models.PositiveSmallIntegerField()  # Minutes
+    c_type = models.CharField(
+        max_length=8,
+        choices=LESSON_TYPE,
+    )
+    room = models.ForeignKey(
+        Room,
+        on_delete=models.CASCADE,
+    )
+
+
+class Shift(models.Model):
+    code = models.CharField(max_length=10)
+    vacancies = models.PositiveSmallIntegerField()
+    lesson_spec = models.ForeignKey(
+        LessonSpecification,
+        on_delete=models.CASCADE,
+    )
+
+
+class Lesson(models.Model):
+    date = models.DateField()
+    lesson_spec = models.ForeignKey(
+        LessonSpecification,
+        on_delete=models.CASCADE,
+    )
+
+
+class Class(models.Model):
+    code = models.CharField(max_length=10)
+    designation = models.CharField(max_length=100)
+    shifts = models.ManyToManyField(Shift)
