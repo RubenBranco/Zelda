b0d94183731f9097fdb3546daf88be3e74c2d343
Ruben Branco <ruben.branco@outlook.pt>
2019-03-06 17:38:38 +0000

    Changing staticfiles_dirs to reflect current structure.

--- src/zelda/zelda/settings.py
+++ src/zelda/zelda/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -124,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'deps', 'static'),
+]
