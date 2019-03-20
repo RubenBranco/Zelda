230605e3119a6c81042f7dc26619022d0fe12c91
Ruben Branco <ruben.branco@outlook.pt>
2019-03-20 23:12:39 +0000

    LanguageSwitch refactor & connection.

--- src/zelda/deps/src/Components/LanguageSwitch.jsx
+++ src/zelda/deps/src/Components/LanguageSwitch.jsx
@@ -1,53 +1,54 @@
 import React from "react";
-///simport Button from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 class LanguageSwitch extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      language: "PT"
-    };
+    constructor() {
+        super();
+        this.state = {};
 
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(event) {
-    if (this.state.language == "PT") {
-      this.setState({
-        language: "EN"
-      });
-    } else {
-      this.setState({
-        language: "PT"
-      });
+        this.handleClick = this.handleClick.bind(this);
+        this.languages = [window.currentLanguage, ...window.otherLanguages];
     }
-    console.log(this.state);
-  }
 
-  render() {
-    return (
-      <Form id="formLanguageSwitch">
-        <button
-          variant="primary"
-          class="btn button_form_switch "
-          type="submit"
-          onClick={this.handleClick}
-        >
-          PT
-        </button>
-        /
-        <button
-          variant="primary"
-          class="btn button_form_switch"
-          type="submit"
-          onClick={this.handleClick}
-        >
-          EN
-        </button>
-      </Form>
-    );
-  }
+    handleClick(event) {
+        let payload = {
+            'csrfmiddlewaretoken': document.getElementsByName("csrfmiddlewaretoken")[0].value,
+            'next': '',
+            'language': event.target.value,
+        };
+        fetch(window.changeLangUrl, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            },
+            body: Object.entries(payload).map(([key, value]) =>
+            encodeURIComponent(key) + '=' + encodeURIComponent(value)).join('&'),
+        }).then(function(response) {
+            if (response.ok && response.status === 200) {
+                location.reload();
+            }
+        });
+    }
+
+    render() {
+        return (
+            <Form id="formLanguageSwitch">
+                {this.languages.map((language) =>
+                    <Button
+                        variant="primary"
+                        class="btn button_form_switch"
+                        type="button"
+                        onClick={this.handleClick}
+                        size="sm"
+                        value={language.code}
+                    >
+                        {language.name}
+                    </Button>
+                )}
+            </Form>
+        );
+    }
 }
 
 export default LanguageSwitch;
