020d14b8f01097dcd846a372969295ed10046fcd
Diogo Fernandes <diogo_fernandes98@sapo.pt>
2019-03-20 16:59:51 +0000

    continued serialization and viewsets dev

--- src/zelda/users/serializers.py
+++ src/zelda/users/serializers.py
@@ -1,15 +1,16 @@
-from timetable.models import Attendance
 from rest_framework.serializers import ModelSerializer
+
+from timetable.models import Attendance
 from .models import Student
 
-class AttendanceSerializer (ModelSerializer):
+class AttendanceSerializer(ModelSerializer):
     class Meta:
         model = Attendance
-        fields = "__all__"
+        fields = "__all__" #student, lesson
 
 
 
-class StudentSerializer (ModelSerializer):
+class StudentSerializer(ModelSerializer):
     class Meta:
         model = Student
         fields = ("number", "app_user")
--- src/zelda/users/views.py
+++ src/zelda/users/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import viewsets
+
+from timetable.models import Attendance
+from .models import Student
+from .serializers import AttendanceSerializer, StudentSerializer
 
 # Create your views here.
+
+class AttendanceViewSet(viewsets.ModelViewSet):
+
+    queryset = Attendance.objects.all()
+    serializer_class = AttendanceSerializer
+    #permission_class = (nao sei qual)
+
+    def partial_update(self, request):
+        attendance = get_object_or_404(Attendance)
+        serializer = AttendanceSerializer(attendance, data=request.data)
+        pass
+ 
+
+class StudentViewSet(viewsets.ModelViewSet):
+
+    #queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get_queryset(self):
+        queryset = Attendance.objects.get().student
+        return queryset
+        
+
