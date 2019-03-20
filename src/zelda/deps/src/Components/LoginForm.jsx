90726f2655fb45a4105925bde283b479cd4b9e8a
Ruben Branco <ruben.branco@outlook.pt>
2019-03-20 23:12:16 +0000

    Sending csrfmiddlewaretoken throgh header & body.

--- src/zelda/deps/src/Components/LoginForm.jsx
+++ src/zelda/deps/src/Components/LoginForm.jsx
@@ -2,97 +2,100 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
-import Image from "react-bootstrap/Image";
+
 
 class LoginForm extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.props = props;
-    this.state = {
-      email: "",
-      password: "",
-      error: ""
-    };
+        this.props = props;
+        this.state = {
+            email: "",
+            password: "",
+            error: ""
+        };
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-  login() {
-    fetch(this.props.url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-      },
-      body: JSON.stringify({
-        email: this.state.email,
-        password: this.state.password
-      })
-    })
-      .then(function(response) {
-        if (response.hasOwnProperty("next")) {
-          location.change(response.next);
-        } else {
-          this.setState({ error: gettext("Invalid credentials.") });
-        }
-      })
-      .catch(function(error) {
+    login() {
+        let csrfmiddlewaretoken = document.getElementsByName("csrfmiddlewaretoken")[0].value;
+        fetch(this.props.url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "X-CSRFToken": csrfmiddlewaretoken,
+            },
+            body: JSON.stringify({
+                email: this.state.email,
+                password: this.state.password,
+                csrfmiddlewaretoken: csrfmiddlewaretoken,
+            })
+        })
+            .then(function (response) {
+                if (response.hasOwnProperty("next")) {
+                    location.change(response.next);
+                } else {
+                    this.setState({ error: gettext("Invalid credentials.") });
+                }
+            })
+            .catch(function (error) {
+                this.setState({
+                    error: gettext("Unable to connect. Try again later.")
+                });
+            });
+    }
+
+    handleChange(event) {
         this.setState({
-          error: gettext("Unable to connect. Try again later.")
+            [event.target.name]: event.target.value
         });
-      });
-  }
+    }
 
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  }
+    handleSubmit(event) {
+        event.preventDefault();
+        this.login();
+    }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    this.login();
-  }
+    render() {
+        return (
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Group>
+                    <Form.Label>Email: </Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="email"
+                        placeholder={gettext("Enter email")}
+                        onChange={this.handleChange}
+                    />
+                </Form.Group>
 
-  render() {
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <Form.Group>
-          <Form.Label>Email: </Form.Label>
-          <Form.Control
-            type="text"
-            name="email"
-            placeholder={gettext("Enter email")}
-            onChange={this.handleChange}
-          />
-        </Form.Group>
+                <Form.Group>
+                    <Form.Label>Password: </Form.Label>
+                    <Form.Control
+                        type="password"
+                        name="password"
+                        placeholder={gettext("Password")}
+                        onChange={this.handleChange}
+                    />
+                </Form.Group>
 
-        <Form.Group>
-          <Form.Label>Password: </Form.Label>
-          <Form.Control
-            type="password"
-            name="password"
-            placeholder={gettext("Password")}
-            onChange={this.handleChange}
-          />
-        </Form.Group>
+                <Button
+                    variant="primary"
+                    className="btn btn-primary btn-lg btn-block button_login "
+                    type="submit"
+                >
+                    {gettext("Login")}
+                </Button>
 
-        <Button
-          variant="primary"
-          className="btn btn-primary btn-lg btn-block button_login "
-          type="submit"
-        >
-          {gettext("Login")}
-        </Button>
-
-        {this.state.error ? (
-          <Alert variant="danger">{this.state.error}</Alert>
-        ) : null}
-      </Form>
-    );
-  }
+                {this.state.error ? (
+                    <Alert variant="danger">{this.state.error}</Alert>
+                ) : null}
+            </Form>
+        );
+    }
 }
 
 export default LoginForm;
