fa05028a44588a5e0ffe0db780ace1acad2459ab
Ruben Branco <ruben.branco@outlook.pt>
2019-03-20 12:37:29 +0000

    Using re substitution instead of rstrip

--- src/zelda/common/views.py
+++ src/zelda/common/views.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from django.shortcuts import render, reverse, redirect
 from django.http.response import HttpResponse
@@ -17,13 +18,13 @@ class AbstractAppView(TemplateView):
         super().__init_subclass__(**kwargs)
         cls_name = cls.__name__
         if not cls.__name__.startswith("Abstract"):
-            cls.template_name = f"zelda/{cls_name.strip('View').lower()}.html"
+            cls.template_name = f"zelda/{re.sub('View', '', cls_name).lower()}.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update(
             dict(
-                view_name=self.__class__.__name__.strip('View').lower(),
+                view_name=re.sub("View", "", self.__class__.__name__).lower(),
             )
         )
         return context
