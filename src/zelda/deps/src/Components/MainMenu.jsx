6a6ce64215f6f7b7590311733db4252845980333
diogoku <diogomnribeiro@gmail.com>
2019-03-27 13:10:08 +0000

    frontpage style.

--- src/zelda/deps/src/Components/MainMenu.jsx
+++ src/zelda/deps/src/Components/MainMenu.jsx
@@ -1,65 +1,98 @@
-import React from 'react';
-import CardDeck from 'react-bootstrap/CardDeck';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import InputGroup from 'react-bootstrap/InputGroup';
-import FormControl from 'react-bootstrap/FormControl';
+import React from "react";
+import CardDeck from "react-bootstrap/CardDeck";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import ListGroup from "react-bootstrap/ListGroup";
+import Image from "react-bootstrap/Image";
 
-class MainMenu extends React.Component{
-    render (){
-        return (
-            <div id="mainMenu">
-                <CardDeck>
-                    <Card>
-                        <Card.Body>
-                        <Card.Title>{gettext("Subjects")}</Card.Title>
-                        <Card.Text>
-                            <Button variant="secondary" size="sm" block>{gettext("Courses")}</Button>
-                        </Card.Text>
-                        </Card.Body>
-                        <Card.Footer/>
-                    </Card>
-                    <Card>
-                        <Card.Body>
-                        <Card.Title>{gettext("Consult")}</Card.Title>
-                        <Card.Text>
-                            <Button variant="secondary" size="sm" block>{gettext("Attendance")}</Button>
-                            <Button variant="secondary" size="sm" block>{gettext("Curriculum")}</Button>
-                            <Button variant="secondary" size="sm" block>{gettext("Profile")}</Button>
-                        </Card.Text>
-                        </Card.Body>
-                        <Card.Footer/>
-                    </Card>
-                    <Card>
-                        <Card.Body>
-                        <Card.Title>{gettext("Schedule")}</Card.Title>
-                        <Card.Text>
-                            <Button variant="secondary" size="sm" block>{gettext("Personal")}</Button>
-                            <Button variant="secondary" size="sm" block>{gettext("By Subjects")}</Button>
-                            <Button variant="secondary" size="sm" block>{gettext("Enroll / Unsubscribe in Curricular Units")}</Button>
-                            <Button variant="secondary" size="sm" block>{gettext("Enroll / Unsubscribe in Shifts")}</Button>
-                        </Card.Text>
-                        </Card.Body>
-                        <Card.Footer/>
-                    </Card>
-                </CardDeck>
+class MainMenu extends React.Component {
+  render() {
+    return (
+      <div>
+        <div id="img">
+          <Image src="../static/img/background.png" id="img_background_all" />
+        </div>
+        <div id="mainMenu">
+          <CardDeck>
+            <Card>
+              <Card.Header className="text-center cardheader font-weight-bold">
+                {gettext("Subjects")}
+              </Card.Header>
+              <Card.Body>
+                <Card.Text>
+                  <ListGroup variant="flush">
+                    <ListGroup.Item action>{gettext("Courses")}</ListGroup.Item>
+                  </ListGroup>
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer />
+            </Card>
+            <Card>
+              <Card.Header className="text-center font-weight-bold">
+                {gettext("Consult")}
+              </Card.Header>
+              <Card.Body>
+                <Card.Text>
+                  <ListGroup variant="flush">
+                    <ListGroup.Item action>
+                      {gettext("Attendance")}
+                    </ListGroup.Item>
+                    <ListGroup.Item action>
+                      {gettext("Curriculum")}
+                    </ListGroup.Item>
+                    <ListGroup.Item action>{gettext("Profile")}</ListGroup.Item>
+                  </ListGroup>
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer />
+            </Card>
+            <Card>
+              <Card.Header className="text-center font-weight-bold">
+                {gettext("Schedule")}
+              </Card.Header>
+              <Card.Body>
+                <Card.Text>
+                  <ListGroup variant="flush">
+                    <ListGroup.Item action>
+                      {gettext("Personal")}
+                    </ListGroup.Item>
+                    <ListGroup.Item action>
+                      {gettext("By Subjects")}
+                    </ListGroup.Item>
+                    <ListGroup.Item action>
+                      {gettext("Enroll / Unsubscribe in Curricular Units")}
+                    </ListGroup.Item>
+                    <ListGroup.Item action>
+                      {gettext("Enroll / Unsubscribe in Shifts")}
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer />
+            </Card>
+          </CardDeck>
 
-                <div id="searchProfile">
-                    <label htmlFor="basic-url">{gettext("Search Profile:")}</label>
-                    <InputGroup className="mb-3">
-                        <FormControl
-                        placeholder={gettext("Search Profile")}
-                        aria-label="Recipient's username"
-                        aria-describedby="basic-addon2"
-                        />
-                        <InputGroup.Append>
-                        <Button variant="outline-secondary">{gettext("Search")}</Button>
-                        </InputGroup.Append>
-                    </InputGroup>
-                </div>
-            </div>
-        )
-    }
+          <div id="searchProfile">
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder={gettext("Search Profile")}
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <InputGroup.Append>
+                <Button variant="outline-secondary" className="searchButton">
+                  {" "}
+                  <i class="fas fa-search" />
+                </Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MainMenu;
--- src/zelda/deps/src/Components/Navigator.jsx
+++ src/zelda/deps/src/Components/Navigator.jsx
@@ -1,57 +1,53 @@
 import React from "react";
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Image from 'react-bootstrap/Image';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Image from "react-bootstrap/Image";
 
+class Navigator extends React.Component {
+  render() {
+    return (
+      <Navbar id="navbar">
+        <Nav className="mr-auto">
+          <Navbar.Brand href={window.frontpageUrl}>
+            <Image
+              alt=""
+              src={window.fculLogoBranco}
+              width="30"
+              height="30"
+              className="d-inline-block align-top navLogoImages"
+            />
+          </Navbar.Brand>
+        </Nav>
+        <Nav>
+          <Image
+            alt=""
+            src={window.userImageUrl}
+            width="30"
+            height="30"
+            className="d-inline-block align-top navImage"
+          />
 
-class Navigator extends React.Component{
-    render(){
-        return(
-            <Navbar id="navbar">
-                <Nav className="mr-auto">
-                    <Navbar.Brand href={window.frontpageUrl}>
-                        <Image
-                            alt=""
-                            src={window.zeldaLogo}
-                            width="30"
-                            height="30"
-                            className="d-inline-block align-top navLogoImages"
-                        />
-                        <Image
-                            alt=""
-                            src={window.fculLogo}
-                            width="30"
-                            height="30"
-                            className="d-inline-block align-top navLogoImages"
-                        />
-                    </Navbar.Brand>
-                    
-                    <Image
-                        alt=""
-                        src={window.userImageUrl}
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-top navImage"
-                    />
+          <Navbar.Text className="nomedoUser">ANA HABRKAKDMASDAS</Navbar.Text>
+        </Nav>
 
-                    <Navbar.Text>
-                        {window.firstName} {window.lastName}
-                    </Navbar.Text>
-
-                    <Nav.Link className="navlinks" href={window.logoutUrl}>Logout</Nav.Link>
-                    <Nav.Link className="navlinks" href={window.webmailUrl} target="_blank">
-                        <Image
-                            alt=""
-                            src={window.messageIcon}
-                            width="30"
-                            height="30"
-                            className="d-inline-block align-top navImage"
-                        />
-                    </Nav.Link>
-                </Nav>
-             </Navbar>
-        )
-    }
+        <Nav className="ml-auto">
+          <Navbar.Text>
+            {window.firstName} {window.lastName}
+          </Navbar.Text>
+          <Nav.Link
+            className="navlinks"
+            href={window.webmailUrl}
+            target="_blank"
+          >
+            <i class="far fa-envelope" />
+          </Nav.Link>
+          <Nav.Link className="navlinks" href={window.logoutUrl}>
+            <i class="fas fa-sign-out-alt" />
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+    );
+  }
 }
 
 export default Navigator;
