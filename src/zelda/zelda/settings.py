59642722b318978933119c89e2d390169c76047e
Ruben Branco <ruben.branco@outlook.pt>
2019-03-30 23:10:21 +0000

    Separating db locations in prod and dev env.

--- src/zelda/zelda/settings.py
+++ src/zelda/zelda/settings.py
@@ -84,12 +84,20 @@ WSGI_APPLICATION = 'zelda.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(os.getenv("DATA_DIR"), 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
