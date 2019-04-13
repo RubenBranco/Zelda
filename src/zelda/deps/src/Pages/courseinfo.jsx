e5736c200f09674a884b7b562f01cc453eab62a9
Ruben Branco <ruben.branco@outlook.pt>
2019-04-13 20:03:49 +0100

    Removing ViewCurricularUnit component from wrong place.

--- src/zelda/deps/src/Pages/courseinfo.jsx
+++ src/zelda/deps/src/Pages/courseinfo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import Navigator from "../Components/Navigator.jsx";
-import ViewCurricularUnitData from "../Components/ViewCurricularUnitData.jsx";
+
 
 class Courseinfo extends React.Component{
     constructor() {
@@ -13,9 +13,8 @@ class Courseinfo extends React.Component{
         return (
          <div>
             <Navigator />
-            <ViewCurricularUnitData />
-          </div> 
-        );        
+          </div>
+        );
     }
 }
 
