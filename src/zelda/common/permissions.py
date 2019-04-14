156a9da490cd88bfb7cc7d6d4c7b00a4961f5bdb
Ruben Branco <ruben.branco@outlook.pt>
2019-04-14 19:18:50 +0100

    Adding IsAuthenticated as fallback for base permission.

--- src/zelda/common/permissions.py
+++ src/zelda/common/permissions.py
@@ -1,11 +1,11 @@
-from rest_framework.permissions import BasePermission, IsAdminUser
+from rest_framework.permissions import BasePermission, IsAdminUser, IsAuthenticated
 
 
 class BaseAppPermission(BasePermission):
     def has_permission(self, request, view):
         if view.action == "list":
             return IsAdminUser().has_permission(request, view)
-        return True
+        return IsAuthenticated().has_permission(request, view)
 
     def has_object_permission(self, request, view, obj):
         return obj == request.user or request.user.is_superuser
