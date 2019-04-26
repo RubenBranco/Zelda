f8f302a055aa40078666eff64241f9b7d238dd39
Ruben Branco <ruben.branco@outlook.pt>
2019-04-26 22:09:02 +0100

    Bugfixing zelda_s3

--- src/zelda_s3/user_uploads/views.py
+++ src/zelda_s3/user_uploads/views.py
@@ -1,11 +1,15 @@
 import os
 import json
 
-from django.views.generic import View
+from django.views import View
+from django.http.response import HttpResponse
 from django.http.request import QueryDict
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class UserImageView(View):
     def post(self, request):
         base_path = os.path.join(settings.MEDIA_ROOT, request.POST['path'])
@@ -14,6 +18,7 @@ class UserImageView(View):
             request.FILES['image'],
             os.path.join(base_path, request.FILES['image'].name),
         )
+        return HttpResponse(status=200)
 
     def save_file(self, f, path):
         with open(path, 'wb+') as destination:
