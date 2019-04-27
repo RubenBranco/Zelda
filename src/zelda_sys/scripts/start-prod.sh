03f16dc563b25650eafaca52d4da63a9e1e5af8a
Ruben Branco <ruben.branco@outlook.pt>
2019-04-27 23:57:38 +0100

    Zelda sys service (just db backup for now)

---
+++ src/zelda_sys/scripts/start-prod.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+
+cd $PROJECT_DIR
+python3 manage.py runcrons
+gunicorn \
+    --bind 0.0.0.0:8000 \
+    --pid $PIDS_DIR/gunicorn.pid \
+    --access-logfile $LOGS_DIR/access.log \
+    --error-logfile $LOGS_DIR/error.log \
+    --capture-output \
+    --name zelda_sys-gunicorn \
+    zelda_sys.wsgi:application
