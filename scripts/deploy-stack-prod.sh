219da6b9530a4166b3d0de6ca6dddd1b7e654bad
Ruben Branco <ruben.branco@outlook.pt>
2019-05-08 22:21:24 +0100

    Bugfix + reducing replicas

--- scripts/deploy-stack-prod.sh
+++ scripts/deploy-stack-prod.sh
@@ -2,5 +2,6 @@
 
 export DJANGO_REPLICAS=6
 export DJANGO_S3_REPLICAS=2
+export RABBITMQ_REPLICAS=2
 
 docker stack deploy -c docker/compose/docker-compose-prod.yml zelda
--- src/zelda/scripts/functions.sh
+++ src/zelda/scripts/functions.sh
@@ -18,7 +18,7 @@ function start_dev {
 }
 
 function start_celery {
-    celery -A zelda beat -s /zelda/celery/ &
+    celery -A zelda beat -s /zelda/celery/celerybeat-schedule &
 }
 
 function migrate {
