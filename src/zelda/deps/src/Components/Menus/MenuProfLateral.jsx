f2bb1c5f1d57df328237dd97eee300fe9b2668d4
Ana Pinheiro <anapinheiro@MBP-de-Ana.home>
2019-06-06 22:23:35 +0100

    mudado sem coisas a mais

--- src/zelda/deps/src/Components/Menus/MenuProfLateral.jsx
+++ src/zelda/deps/src/Components/Menus/MenuProfLateral.jsx
@@ -12,14 +12,12 @@ class MenuProfLateral extends React.Component {
             <div class="menu-prof">
             <Dropdown.Menu show className="MenuProfLateral">
                 <Dropdown.Header>{gettext("Cadeiras")}</Dropdown.Header>
-                <Dropdown.Item eventKey="1">Another action!</Dropdown.Item>
-                <Dropdown.Item eventKey="2">Something else</Dropdown.Item>
+              
                 <Dropdown.Header>Consultar</Dropdown.Header>
                 <Dropdown.Item eventKey="4">{gettext("Presenças")}</Dropdown.Item>
                 <Dropdown.Item eventKey="5">{gettext("Perfil")}</Dropdown.Item>
                 <Dropdown.Header>Requests</Dropdown.Header>
-                <Dropdown.Item eventKey="6">{gettext("Room Change")}</Dropdown.Item>
-                <Dropdown.Item eventKey="7">{gettext("Room Reservation")}</Dropdown.Item>
+        
             </Dropdown.Menu>
             </div>
         );
