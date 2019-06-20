ee6f5f78299341eea1f5919b566b1a5f6b831175
Ruben Branco <ruben.branco@outlook.pt>
2019-06-20 21:53:17 +0100

    Changing the host on locust launch.

--- tests/start_locust_swarm.sh
+++ tests/start_locust_swarm.sh
@@ -3,6 +3,6 @@
 python3 tests/auth.py 9022 tests/student_auth.txt tests/prof_auth.txt &
 echo $! > tests/auth.pid
 
-locust -f tests/locustfile.py --master --host=http://localhost:8000 > tests/master.log 2>&1 &
-locust -f tests/locustfile.py --slave --host=http://localhost:8000 &
-locust -f tests/locustfile.py --slave --host=http://localhost:8000 &
+locust -f tests/locustfile.py --master --host=https://zelda-edu.me > tests/master.log 2>&1 &
+locust -f tests/locustfile.py --slave --host=https://zelda-edu.me &
+locust -f tests/locustfile.py --slave --host=https://zelda-edu.me &
