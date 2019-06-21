ce59ab91ea0890c038610c84ae9ebb2b9b6b1fce
diogoku <diogomnribeiro@gmail.com>
2019-06-21 15:36:01 +0100

    Lateral Menus linked! MIssing profile

--- src/zelda/deps/src/Components/Menus/MenuProfLateral.jsx
+++ src/zelda/deps/src/Components/Menus/MenuProfLateral.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import Image from "react-bootstrap/Image";
 import Dropdown from 'react-bootstrap/Dropdown'
-import DropdownButton from 'react-bootstrap/DropdownButton'
+
 class MenuProfLateral extends React.Component {
     constructor(props) {
         super();
@@ -11,13 +10,12 @@ class MenuProfLateral extends React.Component {
         return (
             <div class="menu-prof">
             <Dropdown.Menu show className="MenuProfLateral">
-                <Dropdown.Header>{gettext("Subjects")}</Dropdown.Header>
-
                 <Dropdown.Header>{gettext("Consult")}</Dropdown.Header>
-                <Dropdown.Item eventKey="4">{gettext("Attendances")}</Dropdown.Item>
-                <Dropdown.Item eventKey="5">{gettext("Profile")}</Dropdown.Item>
+                <Dropdown.Item eventKey="1" href={window.viewAttendancesUrl}>{gettext("Attendances")}</Dropdown.Item>
+                <Dropdown.Item eventkey="2" href={window.viewShiftlessStudentsUrl}>{gettext("Shiftless Students")}</Dropdown.Item>
+                <Dropdown.Item eventKey="3" href={window.viewProfileUrl}>{gettext("Profile")}</Dropdown.Item>
                 <Dropdown.Header>{gettext("Requests")}</Dropdown.Header>
-                <Dropdown.Item eventKey="6">{gettext("Shift Exchange Requests")}</Dropdown.Item>
+                <Dropdown.Item eventKey="4" href={window.viewShiftRequestsUrl}>{gettext("Shift Exchange Requests")}</Dropdown.Item>
             </Dropdown.Menu>
             </div>
         );
