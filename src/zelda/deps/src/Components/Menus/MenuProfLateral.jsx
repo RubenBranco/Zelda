8e8d376c5096510d980cdadcd2782c3bd307f909
Ana Pinheiro <anapinheiro@MBP-de-Ana.home>
2019-06-29 22:52:25 +0100

    menus continuacao

--- src/zelda/deps/src/Components/Menus/MenuProfLateral.jsx
+++ src/zelda/deps/src/Components/Menus/MenuProfLateral.jsx
@@ -15,7 +15,7 @@ class MenuProfLateral extends React.Component {
                     <Dropdown.Item eventKey="1" href={window.viewAttendancesUrl}>{gettext("Attendances")}</Dropdown.Item>
                     <Dropdown.Item eventkey="2" href={window.viewShiftlessStudentsUrl}>{gettext("Shiftless Students")}</Dropdown.Item>
                     <Dropdown.Item eventKey="3" href={window.viewProfileUrl}>{gettext("Profile")}</Dropdown.Item>
-                    <Dropdown.Item eventKey="5" href={window.uploadGradesUrl}>{gettext("Upload Grandes")}</Dropdown.Item>
+                    <Dropdown.Item eventKey="5" href={window.uploadGradesUrl}>{gettext("Upload Grades")}</Dropdown.Item>
                     <Dropdown.Item eventKey="4" href={window.viewShiftRequestsUrl}>{gettext("Shift Exchange Requests")}</Dropdown.Item>
                 </Dropdown.Menu>
             </div>
--- src/zelda/deps/src/Components/Menus/MenuStudLateral.jsx
+++ src/zelda/deps/src/Components/Menus/MenuStudLateral.jsx
@@ -11,12 +11,12 @@ class MenuStudLateral extends React.Component {
             <Dropdown.Menu show className="MenuStudLateral">
                 <Dropdown.Header>{gettext("Consult")}</Dropdown.Header>
                 <Dropdown.Item eventKey="1" href={window.viewAttendancesUrl}>{gettext("Attendances")}</Dropdown.Item>
-                <Dropdown.Item eventKey="2" href="#">{gettext("Curriculum")}</Dropdown.Item>
+                <Dropdown.Item eventKey="2" href={window.viewCurriculumUrl}>{gettext("Curriculum")}</Dropdown.Item>
                 <Dropdown.Item eventKey="3" href={window.viewGradesUrl}>{gettext("Grades")}</Dropdown.Item>
                 <Dropdown.Item eventKey="4" href={window.viewProfileUrl}>{gettext("Profile")}</Dropdown.Item>
                 <Dropdown.Item eventKey="5" href={window.viewTimetableUrl}>{gettext("Schedule")}</Dropdown.Item>
                 <Dropdown.Header>{gettext("Management")}</Dropdown.Header>
-                <Dropdown.Item eventKey="6" href="#">{gettext("Curricular Units")}</Dropdown.Item>
+                <Dropdown.Item eventKey="6" href={window.viewSubjectSignupUrl}>{gettext("Curricular Units")}</Dropdown.Item>
                 <Dropdown.Item eventKey="7" href={window.viewShiftManagementUrl}>{gettext("Shifts")}</Dropdown.Item>
             </Dropdown.Menu>
         );
