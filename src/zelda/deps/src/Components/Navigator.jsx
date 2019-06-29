2936ab714c0fd57ccb30efc6c3838201d1cf3e2c
Ruben Branco <ruben.branco@outlook.pt>
2019-06-29 17:02:22 +0100

    Fixing navigator.

--- src/zelda/deps/src/Components/Navigator.jsx
+++ src/zelda/deps/src/Components/Navigator.jsx
@@ -3,54 +3,56 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Image from "react-bootstrap/Image";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faSignOutAlt} from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import LanguageSwitch from './LanguageSwitch.jsx';
 import Navitem from 'react-bootstrap/NavLink';
 import NavLink from "react-bootstrap/NavLink";
 
 
 class Navigator extends React.Component {
-  render() {
-    return (
-      <Navbar id="navbar" collapseOnSelect expand="sm">
-        <Nav className="mr-auto">
-          <Navbar.Brand href={window.frontpageUrl}>
-            <Image
-              alt=""
-              src={window.fculLogoBranco}
-              width="30"
-              height="30"
-              className="d-inline-block align-top navLogoImages"
-            />
-          </Navbar.Brand>
-        </Nav>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ml-auto">
-              <Image
-                alt=""
-                src={window.userImageUrl}
-                width="30"
-                height="30"
-                className="d-inline-block align-top navImage"
-              />
-              <Navbar.Text className="nomedoUser">{window.firstName} {window.lastName}</Navbar.Text>
-              <LanguageSwitch />
-              <a
-                className="navlinks"
-                href={window.webmailUrl}
-                target="_blank"
-              >
-                <FontAwesomeIcon icon={faEnvelope} />
-              </a>
-              <a className="navlinks" href={window.logoutUrl}>
-                <FontAwesomeIcon icon={faSignOutAlt} />
-              </a>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
+    render() {
+        return (
+            <Navbar id="navbar" collapseOnSelect expand="sm">
+                <Nav className="mr-auto">
+                    <Navbar.Brand href={window.frontpageUrl}>
+                        <Image
+                            alt=""
+                            src={window.fculLogoBranco}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top navLogoImages"
+                        />
+                    </Navbar.Brand>
+                    <Nav.Item>
+                        <Image
+                            alt=""
+                            src={window.userImageUrl}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top navImage"
+                        />
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Navbar.Text className="nomedoUser">{window.firstName} {window.lastName}</Navbar.Text>
+                    </Nav.Item>
+                </Nav>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Item>
+                            <LanguageSwitch />
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href={window.webmailUrl} target="_blank"><FontAwesomeIcon icon={faEnvelope} /></Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href={window.logoutUrl}><FontAwesomeIcon icon={faSignOutAlt} /></Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
 }
 
 export default Navigator;
