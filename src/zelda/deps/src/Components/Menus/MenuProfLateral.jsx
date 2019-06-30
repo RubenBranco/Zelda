ce9645d197f7ce30ee402b360fe4b7c4891e5a67
Ana Pinheiro <anapinheiro@MacBook-Pro-de-Ana.local>
2019-06-30 14:53:54 +0100

    merge

--- src/zelda/deps/src/Components/Menus/MenuProfLateral.jsx
+++ src/zelda/deps/src/Components/Menus/MenuProfLateral.jsx
@@ -11,12 +11,12 @@ class MenuProfLateral extends React.Component {
             <div className="menu-prof">
                 <Dropdown.Menu show className="MenuProfLateral">
                     <Dropdown.Header>{gettext("Consult")}</Dropdown.Header>
-                    <Dropdown.Item eventKey="6" href={window.viewShiftStatusUrl}>{gettext("Shift Statuses")}</Dropdown.Item>
-                    <Dropdown.Item eventKey="1" href={window.viewAttendancesUrl}>{gettext("Attendances")}</Dropdown.Item>
-                    <Dropdown.Item eventkey="2" href={window.viewShiftlessStudentsUrl}>{gettext("Shiftless Students")}</Dropdown.Item>
-                    <Dropdown.Item eventKey="3" href={window.viewProfileUrl}>{gettext("Profile")}</Dropdown.Item>
-                    <Dropdown.Item eventKey="5" href={window.uploadGradesUrl}>{gettext("Upload Grades")}</Dropdown.Item>
-                    <Dropdown.Item eventKey="4" href={window.viewShiftRequestsUrl}>{gettext("Shift Exchange Requests")}</Dropdown.Item>
+                    <Dropdown.Item eventKey="1" href={window.viewShiftStatusUrl}>{gettext("Shift Statuses")}</Dropdown.Item>
+                    <Dropdown.Item eventKey="2" href={window.viewAttendancesUrl}>{gettext("Attendances")}</Dropdown.Item>
+                    <Dropdown.Item eventkey="3" href={window.viewShiftlessStudentsUrl}>{gettext("Shiftless Students")}</Dropdown.Item>
+                    <Dropdown.Item eventKey="4" href={window.viewProfileUrl}>{gettext("Profile")}</Dropdown.Item>
+                    <Dropdown.Item eventKey="6" href={window.uploadGradesUrl}>{gettext("Upload Grades")}</Dropdown.Item>
+                    <Dropdown.Item eventKey="7" href={window.viewShiftRequestsUrl}>{gettext("Shift Exchange Requests")}</Dropdown.Item>
                 </Dropdown.Menu>
             </div>
         );
