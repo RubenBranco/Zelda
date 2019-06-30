479c2189da2dc2d25d8f70501186dd6a4aaf4c5d
Ruben Branco <ruben.branco@outlook.pt>
2019-06-30 14:51:07 +0100

    Finishing up professor view grades

--- src/zelda/deps/src/Components/Menus/MenuProfLateral.jsx
+++ src/zelda/deps/src/Components/Menus/MenuProfLateral.jsx
@@ -13,8 +13,11 @@ class MenuProfLateral extends React.Component {
                     <Dropdown.Header>{gettext("Consult")}</Dropdown.Header>
                     <Dropdown.Item eventKey="6" href={window.viewShiftStatusUrl}>{gettext("Shift Statuses")}</Dropdown.Item>
                     <Dropdown.Item eventKey="1" href={window.viewAttendancesUrl}>{gettext("Attendances")}</Dropdown.Item>
-                    <Dropdown.Item eventkey="2" href={window.viewShiftlessStudentsUrl}>{gettext("Shiftless Students")}</Dropdown.Item>
+                    <Dropdown.Item eventKey="2" href={window.viewShiftlessStudentsUrl}>{gettext("Shiftless Students")}</Dropdown.Item>
                     <Dropdown.Item eventKey="3" href={window.viewProfileUrl}>{gettext("Profile")}</Dropdown.Item>
+                    <Dropdown.Item eventKey="7" href={window.viewGradesUrl}>
+                        {gettext("View Grades")}
+                    </Dropdown.Item>
                     <Dropdown.Item eventKey="5" href={window.uploadGradesUrl}>{gettext("Upload Grades")}</Dropdown.Item>
                     <Dropdown.Item eventKey="4" href={window.viewShiftRequestsUrl}>{gettext("Shift Exchange Requests")}</Dropdown.Item>
                 </Dropdown.Menu>
